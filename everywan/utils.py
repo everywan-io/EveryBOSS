66f7245dc0b8446915528591a53d8c6573ee803b
Francesco Lombardo <franclombardo@gmail.com>
2020-02-13 12:33:10 +0100

    fix objectid to string

--- everywan/utils.py
+++ everywan/utils.py
@@ -17,6 +17,11 @@ from bson import json_util
 
 
 def mongo_cursor_to_json(mongo_cursor):
-    data = [json.loads(json.dumps(item, default=json_util.default))
+    data = [json.loads(json.dumps(id_to_string(item), default=json_util.default))
             for item in mongo_cursor]
     return data
+
+
+def id_to_string(item):
+    item._id = str(item._id)
+    return item
